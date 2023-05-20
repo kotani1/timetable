3ef58e9738b68455fa52090f9aadd9c932173fed
山地 <21011111@c.anabuki-college.net>
2023-05-20 10:39:58 +0900

    edit

---
+++ timeTable/resources/views/teachers/edit.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <h1>教員登録画面の編集</h1>
+    {{-- <form action="{{route('teachers.update', ['teacher' => $teacher->id])}}" method="post"> --}}
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="login_id"> ログインID</label>
+            <input type="text" name="login_id" id="login_id" class="form-control" value="" placeholder="ログインIDを入力してください">
+        </div>
+        <div class="form-group">
+            <label for="user_name">ユーザーネーム</label>
+            <input type="text" name="user_name" id="user_name" class="form-control" value="" placeholder="ユーザーネームを入力してください">
+        </div>
+        <div class="form-group">
+            <label for="password">パスワード</label>
+            <input type="text" name="password" id="password" class="form-control" value="" placeholder="パスワードを入力してください">
+        </div>
+        <input type="submit" value="決定" class="btn btn-primary">
+        <input type="reset" value="戻る" class="btn btn-secondary" onclick='window.history.back(-1);'>
+    {{-- </form> --}}
+</div>
