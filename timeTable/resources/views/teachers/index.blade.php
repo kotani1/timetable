b795b8c2af80f87990b040993185646ff2ace5bf
山地 <21011111@c.anabuki-college.net>
2023-05-26 09:57:33 +0900

    index

--- timeTable/resources/views/teachers/index.blade.php
+++ timeTable/resources/views/teachers/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>教員登録アプリ</title>
 </head>
 <body>
     <a href="{{route('teachers.create')}}">登録画面へ</a>
