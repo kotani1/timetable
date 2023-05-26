a92c08370c254bc388f52514ac0a797e7b2013ff
山地 <21011111@c.anabuki-college.net>
2023-05-26 12:25:10 +0900

    index

--- timeTable/resources/views/teachers/index.blade.php
+++ timeTable/resources/views/teachers/index.blade.php
@@ -5,41 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/create.css">
     <title>教員登録アプリ</title>
 </head>
+
 <body>
-    <a href="{{route('teachers.create')}}">登録画面へ</a>
-    <h2 class="">
-        登録教員一覧
-    </h2>
 
+  <div class="inner">
+    {{-- <a href="{{route('teachers.create')}}">登録画面へ</a> --}}
+    <button onclick="location.href='{{route('teachers.create')}}'">登録画面へ</button>
+  </div>
 
-<div class="">
-    <div class="">
-        <div class="">
-          <div class="">
+         <div class="inner">
             @if (!$teachers->isEmpty())
                 @foreach ($teachers as $teacher )
-                  {{$teacher ->login_id}}<br>
-                  {{$teacher ->user_name}}<br>
-                  {{$teacher ->permit}}<br>
-                  {{$teacher ->created_at}}<br>
+                  {{-- <p>{{$teacher ->login_id}}</p><br> --}}
+                  <p class="flex">教員名：{{$teacher ->user_name}}</p>
+                  {{-- <p>{{$teacher ->permit}}</p><br> --}}
+                  {{-- <p>{{$teacher ->created_at}}</p><br> --}}
+                  <button onclick="location.href='{{ route('teachers.edit', ['teacher' => $teacher->id])}}'" class="">編集</button>
                   <form id="delete_{{ $teacher->id }}" method="post" action="{{route('teachers.destroy',$teacher->id)}}">
                     @csrf
                     @method('delete')
-                  <td class="px-4 py-3">
+                   <td>
+                    <div class="del">
                     <a href="#" data-id="{{ $teacher->id }}" onclick="deletePost(this)" class="">削除</a>
+                    </div>
                   </td>
                   </form>
                 @endforeach
             @else
                 登録された教員は現在いません。
             @endif
-           </div>
         </div>
-    </div>
-</div>
-
+   
 </body>
 
 <script>
