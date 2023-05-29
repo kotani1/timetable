8094065888e410a050aa4647797289ee98a17fd8
kotani <21011103@c.anabuki-college.net>
2023-05-29 12:11:37 +0900

    aaa

--- timeTable/tailwind.config.js
+++ timeTable/tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  content: [],
-=======
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
->>>>>>> eefe113be22326619ec9c280eece4196c7a14a91
   theme: {
     extend: {},
   },
   plugins: [],
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> eefe113be22326619ec9c280eece4196c7a14a91
