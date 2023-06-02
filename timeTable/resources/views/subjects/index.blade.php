4cb2d59fd658e3239dd364e3647b9e299f5d6c82
山地 <21011111@c.anabuki-college.net>
2023-06-02 11:58:59 +0900

    index.blade.php

--- timeTable/resources/views/subjects/index.blade.php
+++ timeTable/resources/views/subjects/index.blade.php
@@ -41,10 +41,10 @@
                   <tr>
                     <td class="px-4 py-3 divide-y divide-light-blue-700">
                       <font style="vertical-align: inherit;">
-                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->subject_name}}</font>
-                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->subject_content}}</font>
-                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->subject_classification}}</font>
-                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->teaching_form}}</font>
+                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->year}}</font>
+                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->name}}</font>
+                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->content}}</font>
+                        <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->classification}}</font>
                         <font class="text-xl" style=" vertical-align: inherit;">{{$subject ->number_of_units}}</font>
                       </font>
                       <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded" onclick="location.href='{{ route('teachers.edit', ['teacher' => $teacher->id])}}'" class="">編集</button>
