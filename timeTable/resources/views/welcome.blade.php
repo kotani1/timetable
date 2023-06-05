b3f065b9bad02286db1a0815c0e7ebec34572125
山地 <21011111@c.anabuki-college.net>
2023-06-05 11:41:53 +0900

    aaa

--- timeTable/resources/views/welcome.blade.php
+++ timeTable/resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
           @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
-        <button onclick="location.href='{{route('teachers.index')}}'" class="ml-12 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">teachers</button>
-        <button onclick="location.href='{{route('subjects.index')}}'" class="ml-12 bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">subject</button>
+        <button onclick="location.href='{{route('teachers.index')}}'" class="ml-8 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">teachers</button>
+        <button onclick="location.href='{{route('subjects.index')}}'" class="ml-8 bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">subject</button>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
