1ef940e4b33de82f2297e1f8af880a57b1fb063e
21011102 <21011102@c.anabuki-college.net>
2023-06-05 12:01:03 +0900

    editについて

--- timeTable/resources/views/subjects/edit.blade.php
+++ timeTable/resources/views/subjects/edit.blade.php
@@ -4,24 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/edit.css">
     <title>科目変更画面</title>
+    <!-- @vite('resources/css/app.css') -->
 </head>
 <body>
+    <h1 class="h1">科目変更画面</h1>
     <form action= ""method="post">
         @csrf
-        <p>user_name: <input type="text" name="user_name"></p>
-        <p>login_id: <input type="text" name="login_id"></p>
-        <p>password: <input type="text" name="password"></p>
-        
-        <p>permit:
-            <select name="permit" id="">
-                <option value="0">0</option>
+        <p class="name">科目名:　 <input type="text" name="subject_name" id="name"></p>
+        <p class="year">年度: 　<input type="text" name="year" id="year"></p>
+        <p class="period">期:　 
+            <select name="period" id="">
+                <option value="前期">前期</option>
+                <option value="後期">後期</option>
+            </select>
+        </p>
+        <p class="number">単位数:　
+            <select name="number_of_units" id="">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
-            </select>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</opti4on>
+            </select> 
         </p>
-        <button type="">送信する</button>
+
+        <p class="classification">科目分類:　 <input type="text" name="subject_classification" id="classification"></p>
+        
+        <p class="content">科目内容:　<textarea rows="10" cols="50" name="subject_content" id="content"></textarea></p>
+        <div class="send">
+            <button  type="" >送信する</button>
+        </div>
     </form>
 
 
