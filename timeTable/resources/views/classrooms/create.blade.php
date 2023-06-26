779a447a86662758d11dc96387c37f1b1e26b15b
kotani <21011103@c.anabuki-college.net>
2023-06-26 12:29:10 +0900

    classroomsのまとめ

--- timeTable/resources/views/classrooms/create.blade.php
+++ timeTable/resources/views/classrooms/create.blade.php
@@ -1,4 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/classroom.create.css">
+    <title>教室管理</title>
+</head>
+<body>
+        <h1>教室管理登録</h1>
 
-        <p><input type="submit" value="登録を行う" class="button"></p>
+        <p><input type="submit" value="戻る" class="modoru"></p>
+
+        <form action="{{route('classrooms.store')}}" method="POST">
+                @csrf
+                <p>教室名</p>
+                <div class="inner">
+                <input type="text" maxlength="100" placeholder="教室名" id="classroom_name" name="classroom_name" class="inner">
+                </div>
+
+                <p>フロア</P>
+                <div class="inner">
+                <input type="text" maxlength="100" placeholder="フロア" id="floor" name="floor" class="floor">
+                </div>
+
+                <p>収納人数</p>
+                <div class="inner">
+                <input type="text" maxlength="100" placeholder="収納人数" id="capacity" name="capacity" class="inner">
+                </div>
+
+                <p><input type="submit" value="登録を行う" class="button"></p>
+        </form>
 </body>
 </html>
--- timeTable/resources/views/classrooms/edit.blade.php
+++ timeTable/resources/views/classrooms/edit.blade.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/department.edit.css">
+    <link rel="stylesheet" href="/css/classroom.edit.css">
     <title>科目変更画面</title>
 </head>
 <body>
     <h1 class="h1" id="test">教室変更画面</h1>
     <div class="return" >
-        <button  onclick="location.href='{{route('departments.index')}}'" class="color">戻る</button>
+        <button  onclick="location.href='{{route('classrooms.index')}}'" class="color">戻る</button>
     </div>
 
 
         <p class="name" id="test">教室名:
-            <input type="text"   font-size: 150%; size="30" name="department_name"  placeholder="クラス名を入力してください" >
+            <input type="text"   font-size: 150%; size="30" name="classroom_name"  placeholder="クラス名を入力してください" >
         </p>
 
         <p class="name" id="test">フロア:
-            <input type="text"   size="30" name="department_name"  placeholder="フロアを入力してください" >
+            <input type="text"   size="30" name="floor"  placeholder="フロアを入力してください" >
         </p>
 
         <p class="name" id="test">収容人数:
-            <input type="text"  size="30" name="department_name"  placeholder="収容人数を入力してください" >
-        </p>    
+            <input type="text"  size="30" name="capacity"  placeholder="収容人数を入力してください" >
+        </p>
 
         <div class="send" id="test">
             <button  type="" class="changecolor">送信する</button>
