77c77b98baee7ae519e0f7130fd3a0d90d3dcbe2
kotani <21011103@c.anabuki-college.net>
2023-06-26 12:35:35 +0900

    departments.create

--- timeTable/resources/views/departments/create.blade.php
+++ timeTable/resources/views/departments/create.blade.php
@@ -4,29 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/classroom.create.css">
-    <title>教室管理</title>
+    <link rel="stylesheet" href="/css/class.create.css">
+    <title>クラス登録画面</title>
 </head>
 <body>
-        <h1>教室管理登録</h1>
-
-        <p><input type="submit" value="戻る" class="modoru"></p>
-
-        <p>教室名</p>
+    <form action="{{route('departments.store')}}" method="POST" onsubmit="return confirm('本当に登録しますか？')">
+        @csrf
+        <p>クラス名</p>
         <div class="inner">
-            <input type="text" maxlength="100" placeholder="教室名" id="classroom_name" name="classroom_name" class="inner">
+            <input type="text" maxlength="100" placeholder="クラス名" id="department_name" name="department_name" class="inner">
         </div>
 
-        <p>フロア</P>
-        <div class="inner">
-            <input type="text" maxlength="100" placeholder="フロア" id="flora" name="flora" class="inner">
-        </div>
-
-        <p>収納人数</p>
-        <div class="inner">
-            <input type="text" maxlength="100" placeholder="収納人数" id="Capacity" name="Capacity" class="inner">
-        </div>
-
-        <p><input type="submit" value="登録を行う" class="button"></p>
+        <p><input type="submit" value="クラス項目登録を行う" class="button"></p>
+    </form>
 </body>
 </html>
