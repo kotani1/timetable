0fa0b3c2b31161431c8a010d36cb0f2e3b5c178d
21011102 <21011102@c.anabuki-college.net>
2023-07-03 11:03:19 +0900

    edit.css

--- timeTable/resources/views/subjects/edit.blade.php
+++ timeTable/resources/views/subjects/edit.blade.php
@@ -4,62 +4,74 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/edit.css">
+    <link rel="stylesheet" href="/css/editclas.css">
     <title>科目変更画面</title>
-    <!-- @vite('resources/css/app.css') -->
+
 </head>
 <body>
-    <h1 class="h1">科目項目変更画面</h1>
+    <h1 class="inner" >科目項目変更画面</h1>
     <form action="{{route('subjects.update',$subject->id)}}" method="POST">
         @csrf
         @method('PUT')
-        <p class="name">科目名:
-            <input type="text" name="subject_name" id="name" value="{{$subject->subject_name}}">
-        </p>
-        <p class="year">年度:
-            <input type="text" name="year" id="year" value="{{$subject->year}}">
-        </p>
-        <p class="period">期:
-            <select name="period" id="">
-                <option value="前期">前期</option>
-                <option value="後期">後期</option>
-            </select>
-        </p>
-        <p class="name">授業形態:
-            <select name="teaching_form" id="">
-                <option value="実習">実習</option>
-                <option value="講義">講義</option>
-            </select>
-        </p>
-        <p class="number">単位数:
-            <select name="number_of_units" id="">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-            </select>
-        </p>
 
-        <p class="classification">科目分類:
-            <input type="text" name="subject_classification" id="classification" value="{{$subject->subject_classification}}">
-        </p>
-
-        <p class="content">科目内容:
-            <textarea rows="10" cols="50" name="subject_content" id="content">{{$subject->subject_content}}
-            </textarea>
-        </p>
-        <div class="send">
-            <button  type="" >送信する</button>
+        <p>科目名</p>
+        <div class="inner" >
+            <input type="text" maxlength="100" placeholder="科目名" id="name" name="subject_name" class="inner" value="{{$subject->subject_name}}">
         </div>
+
+        <p>年度</p>
+        <div class="inner" >
+            <input type="text" maxlength="100" placeholder="年度" id="year" name="year" class="inner" value="{{$subject->year}}">
+        </div>
+
+        <p>期</p>
+        <div class="inner">
+        <select name="period" id="">
+            <option value="前期" class="inner">前期</option>
+            <option value="後期" class="inner">後期</option>
+        </select>
+        </div>
+
+        <p>授業形態</p>
+        <div class="inner">
+        <select name="teaching_form" id="">
+            <option value="実習" class="inner">実習</option>
+            <option value="講義" class="inner">講義</option>
+        </select>
+        </div>
+
+        <p>単位数</p>
+        <div class="inner">
+        <select name="number_of_units" id="">
+            <option value="-" class="inner">-</option>
+            <option value="1" class="inner">1</option>
+            <option value="2" class="inner">2</option>
+            <option value="3" class="inner">3</option>
+            <option value="4" class="inner">4</option>
+            <option value="5" class="inner">5</option>
+            <option value="6" class="inner">6</option>
+            <option value="7" class="inner">7</option>
+            <option value="8" class="inner">8</option>
+            <option value="9" class="inner">9</option>
+            <option value="10" class="inner">10</option>
+        </select>
+        </div>
+
+        <p>科目分類</p>
+        <div class="inner">
+            <input type="text" maxlength="100" placeholder="科目分類" id="classification" name="subject_classification" class="inner" value="{{$subject->subject_classification}}">
+        </div>
+
+        <p>科目内容</p>
+        <div class="inner">
+            <textarea id="content" name="subject_content" cols="50" rows="10" maxlength="50">{{$subject->subject_content}}</textarea>
+        </div>
+
+        <p><input type="submit" value="送信する"></p>
     </form>
 
 
 
 </body>
 </html>
+
