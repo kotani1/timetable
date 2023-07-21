a1d6efc38828b1062c30d08cfaff99b564f164da
山地 <21011111@c.anabuki-college.net>
2023-07-21 12:17:42 +0900

    テスト

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -8,19 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Calendar extends Model
 {
     use HasFactory;
-<<<<<<< HEAD:timeTable/app/Models/Calendar.php
-=======
-<<<<<<<< HEAD:timeTable/app/Models/Subject.php
+
     protected $guarded = ['id', 'created_at'];
-<<<<<<< HEAD:app/Models/Subject.php
 
     public function department_by_teacher()
     {
         return $this->hasOne(DepartmentByTeacher::class);
     }
-=======
->>>>>>> c69e358c21c5ed358324ccbe2ca6dd9fef54e968:timeTable/app/Models/Subject.php
-========
->>>>>>>> 72e26ed945979189523a1a93d4ad10312f6ac3fb:app/Models/Calendar.php
->>>>>>> 72e26ed945979189523a1a93d4ad10312f6ac3fb:app/Models/Calendar.php
+
 }
